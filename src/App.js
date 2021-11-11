4610f2ab51f7f5945f2161a541d3e25bcc7e4818
gabrielmagevski <gabrielmagevski.gs@gmail.com>
2021-11-11 15:45:26 -0300

    Added UseCallback to form, to fix looping in form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import React from 'react';
 
+import Form from './components/Form';
+import Input from './components/Input';
+
 export default function App() {
     return (
         <>
             <h1>Hello Hooks</h1>
+            <h4>Exercícios feitos até agora de:</h4>
+            <ul style={{ listStyle: 'none' }}>
+                <li>useState</li>
+                <li>useEffect</li>
+                <li>useCallback</li>
+            </ul>
+
+            <h1>Using UseCallback in Form</h1>
+            <Form>
+                <span>Name:</span> <Input name="name" /> 
+                <span>Email:</span> <Input name="email" />  
+                <span>Password:</span> <Input name="password" /> 
+                <button type="submit">Send</button>
+            </Form>
         </>
     );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,20 @@
+import { React, useState, useCallback } from 'react'
+
+import FormContext from './FormContext';
+
+export default function Form({ children }) {
+    const [inputs, setInputs] = useState([]);
+
+    const registerField = useCallback((name, ref) => {
+        setInputs(oldInputs => [...oldInputs, { name, ref }]);
+    }, []);
+
+
+    return (
+        <form>
+            <FormContext.Provider value={{ registerField }}>
+                {children}
+            </FormContext.Provider>
+        </form>
+    )
+}
